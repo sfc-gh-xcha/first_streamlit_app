6a1fd7d12be66fdf45225484cadd069b56d713eb
IttyCha <144943703+IttyCha@users.noreply.github.com>
2023-09-15 12:46:39 -0700

    Update streamlit_app.py - addded text entry box
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    streamlit.write('The user entered ', fruit_choice)

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
