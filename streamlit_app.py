f0cdc5260e885dbd6d1f36cadf2fac31e0dacdfc
IttyCha <144943703+IttyCha@users.noreply.github.com>
2023-09-15 12:48:12 -0700

    Update streamlit_app.py move table at end

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 
@@ -36,3 +34,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
